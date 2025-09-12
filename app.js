3d3d83969b5b7471b903b4066b608090dabecde1
karolinysouza <karolinytech@gmail.com>
2025-09-12 17:02:42 -0300

    feat: render tasks with edit and delete options

--- app.js
+++ app.js
@@ -11,7 +11,31 @@ const addTask = () => {
   }
 };
 
-const updateTasksList = () => {};
+const updateTasksList = () => {
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.innerHTML = `
+    <div class="taskItem">
+        <div class="task ${task.completed ? "completed" : ""}">
+            <input type="checkbox" class="checkbox" ${
+              task.completed ? "checked" : ""
+            }/>
+              <p>${task.text}</p>
+        </div>
+        <div class="icons">
+          <img src="./img/edit.png" onClick="editTask(${index})"/>
+          <img src="./img/bin.png"  onClick="deleteTask(${index})"/>
+        </div>
+    </div>
+    `;
+    listItem.addEventListener("change", () => toggleTaskComplete(index));
+    taskList.append(listItem);
+  });
+};
 
 document.getElementById("newTask").addEventListener("click", function (e) {
   e.preventDefault();
