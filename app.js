2db370d1fb5f3aea0a9d5eae3f7ec51339b64427
karolinysouza <karolinytech@gmail.com>
2025-09-22 10:55:44 -0300

    feat: trigger confetti animation when all tasks are completed

--- app.js
+++ app.js
@@ -34,16 +34,20 @@ const editTask = (index) => {
 };
 
 const updateStats = () => {
-  const completeTasks = tasks.filter((task) => task.completed).length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
   const totalTasks = tasks.length;
-  const progress = (completeTasks / totalTasks) * 100;
+  const progress = (completedTasks / totalTasks) * 100;
   const progressBar = document.getElementById("progress");
 
   progressBar.style.width = `${progress}%`;
 
   document.getElementById(
     "numbers"
-  ).innerHTML = `${completeTasks} / ${totalTasks}`;
+  ).innerHTML = `${completedTasks} / ${totalTasks}`;
+
+  if (tasks.length && completedTasks === totalTasks) {
+    blaskConfetti();
+  }
 };
 
 const updateTasksList = () => {
@@ -77,3 +81,45 @@ document.getElementById("newTask").addEventListener("click", function (e) {
 
   addTask();
 });
+
+const blaskConfetti = () => {
+  const count = 200,
+    defaults = {
+      origin: { y: 0.7 },
+    };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+
+  fire(0.2, {
+    spread: 60,
+  });
+
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+};
